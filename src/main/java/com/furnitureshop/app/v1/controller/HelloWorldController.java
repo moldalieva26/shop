ff875876cae9d6dc2b0aeb373e7e9e1514d6b2d6
Meerim <moldalieva26@gmail.com>
2021-01-26 16:00:56 -0500

    initial commit

--- src/main/java/com/furnitureshop/app/v1/controller/HelloWorldController.java
+++ src/main/java/com/furnitureshop/app/v1/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping({ "/hello" })
+	@RequestMapping({ "/" })
 	public String firstPage() {
 		return "Hello World";
 	}
--- src/test/java/com/furniture/shop/webappv1/tests/CartControllerTest.java
+++ src/test/java/com/furniture/shop/webappv1/tests/CartControllerTest.java
@@ -5,15 +5,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
@@ -22,11 +19,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcConfigurer;
-import org.springframework.web.context.WebApplicationContext;
 
-import org.springframework.test.web.servlet.setup.MockMvcConfigurer;
 
 
 @RunWith(SpringRunner.class)
